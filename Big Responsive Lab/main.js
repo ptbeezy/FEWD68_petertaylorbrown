28f26731d0780499a43f4132547d5c9117e5d705
ptbeezy <petertaylor-brown@Peters-MacBook-Air.local>
2015-11-02 17:57:51 -0500

    Responsive Big Lab
    ful lab

--- Big Responsive Lab/main.js
+++ Big Responsive Lab/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $("#lazy-ham").click(function() {
-      $("#nav-container").toggleClass( "navHidden" );
+      $("#nav-container").toggleClass( "expand" );
   });
 })
